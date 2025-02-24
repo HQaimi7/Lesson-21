e1cdd66fa2af8da167afe8bc62e79773479e06d8
HQaimi7 <Muhammad.Khan4@scchs.org.uk>
2025-02-24 20:29:47 +0000

    Tkinter.py

---
+++ Tkinter.py
@@ -0,0 +1,10 @@
+#Import necessary libraries
+from tkinter import *
+
+#Create Window
+window = Tk()
+
+#Select the window title and geometry(size)
+window.title("Welcome to Tkinter")
+window.geometry('400x300')
+window.mainloop()
